136415b252cf681c512b89e65e318190b7efdea6
TheDarkRoot <leejunfan43@gmail.com>
2023-08-13 20:36:51 +0300

    x
    x

--- modules/title.js
+++ modules/title.js
@@ -1,8 +1,8 @@
 function console_title(x) {
     if (process.platform == 'win32') {
-        process.title = x + "SmsBomb v1.0.0 (Alpha)";
+        process.title = x + " / SmsBomb v1.0.0 (Alpha)";
     } else {
-        process.stdout.write('SmsBomb v1.0.0 (Alpha)" + '\x1b\x5c');
+        process.stdout.write('\x1b]2;' + x + " / SmsBomb v1.0.0 (Alpha)" + '\x1b\x5c');
     }
 }
 
