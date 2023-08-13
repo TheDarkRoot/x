f391cb2ceb0af2fbbd30afef3a39ca0ef1d5890a
TheDarkRoot <leejunfan43@gmail.com>
2023-08-13 20:37:55 +0300

    x
    x

--- modules/title.js
+++ modules/title.js
@@ -1,8 +1,8 @@
 function console_title(x) {
     if (process.platform == 'win32') {
-        process.title = x + " / SmsBomb v1.0.0 (Alpha)";
+        process.title = x + " / FastBomber v1.0 - github.com/fastuptime";
     } else {
-        process.stdout.write('\x1b]2;' + x + " / SmsBomb v1.0.0 (Alpha)" + '\x1b\x5c');
+        process.stdout.write('\x1b]2;' + x + " / FastBomber v1.0 - github.com/fastuptime" + '\x1b\x5c');
     }
 }
 
